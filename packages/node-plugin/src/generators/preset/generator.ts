bf65f4179e8b3254e42b24eb907d486b3e813521
Muhammad Muaz <muhammad.muaz@emumba.com>
2024-07-25 19:31:15 +0500

    Add Preset Folder Structure

--- packages/node-plugin/src/generators/preset/generator.ts
+++ packages/node-plugin/src/generators/preset/generator.ts
@@ -25,7 +25,7 @@ export async function presetGenerator(
 
   updateJson(tree, 'package.json', (json) => {
     json.scripts = json.scripts || {};
-    json.scripts.start = 'nodemon --exec ts-node index.ts';
+    json.scripts.start = 'nodemon --exec ts-node server.ts';
     json.scripts.lint = 'eslint . --ext .ts';
     return json;
   });
